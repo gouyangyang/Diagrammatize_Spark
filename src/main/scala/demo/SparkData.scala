acb28fd0b260ec200d1cd50e258dc200f761cc6e
gouyangyangy@163.com <5633878yy>
2018-11-27 21:50:42 +0800

    测试代码

--- src/main/scala/demo/SparkData.scala
+++ src/main/scala/demo/SparkData.scala
@@ -6,22 +6,22 @@ import org.apache.spark.sql.SparkSession
   * @author gouyangyang on 2018/10/15/015 19:59
   */
 object SparkData {
-    def main(args:Array[String]):Unit={
-      val spark=SparkSession.builder().appName("fileRead").getOrCreate()
-      val data1=spark.read
-        // 推断数据类型
-        .option("inferSchema",true)
-        //设置空值
-        .option("nullValue","?")
-        // 表示有表头，若没有则为false
-        .option("header",true)
-        //文件路径
-        .csv("D:\\github_project\\Diagrammatize_Spark\\data\\11.csv")
-        //缓存
-        .cache()
-      //打印数据格式
-      data1.printSchema()
-      //显示数据,false参数为不要把数据截断
-      data1.show(false)
-    }
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("fileRead").getOrCreate()
+    val data1 = spark.read
+      // 推断数据类型
+      .option("inferSchema", true)
+      //设置空值
+      .option("nullValue", "?")
+      // 表示有表头，若没有则为false
+      .option("header", true)
+      //文件路径
+      .csv("D:\\github_project\\Diagrammatize_Spark\\data\\11.csv")
+      //缓存
+      .cache()
+    //打印数据格式
+    data1.printSchema()
+    //显示数据,false参数为不要把数据截断
+    data1.show(false)
+  }
 }
