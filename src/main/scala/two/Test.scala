93f6733db115e426fdd31f5e5327bcade42ccec9
gouyangyang <gouyangyangy@163.com>
2025-12-31 11:02:39 +0800

    LeetCode 1. 两数之和

---
+++ src/main/scala/two/Test.scala
@@ -0,0 +1,13 @@
+package two
+
+/**
+ * @author gouyy
+ * @date 2025/12/31 10:20
+ * @version 1.0
+ */
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
