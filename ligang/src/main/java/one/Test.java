0277d775524c7e80acfaa6223c1eac4dfb051b28
gouyangyang <gouyangyangy@163.com>
2026-04-19 01:50:03 +0800

    ligang code

---
+++ ligang/src/main/java/one/Test.java
@@ -0,0 +1,16 @@
+package one;
+
+public class Test {
+    public static void main(String[] args) {
+        int a= 10;
+        Integer a2 = 10;
+
+        System.out.println(a==a2);
+        System.out.println(a==10);
+        System.out.println(a2==10);
+        int  a3=  Integer.valueOf(10);
+        System.out.println(a3==10);
+
+
+    }
+}
